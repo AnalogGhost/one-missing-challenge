d6cddcab613459b279f5bc9e00f23097a607cd46
Mathew Brown <matt@mattbrown.com>
2017-05-10 10:10:55 -0600

    Add Solution

--- solution.js
+++ solution.js
@@ -2,6 +2,13 @@
 
 module.exports = {
   findMissing: arr => {
+    let originalLength = arr.length + 1;
+    let originalSum = (originalLength * (originalLength + 1)) / 2;
 
+    // 1 + 2 + 3 + 4 + ⋯ + n
+    // https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+    let arrSum = arr.reduce((acc,el) => acc + el);
+
+    return originalSum - arrSum;
   }
 }
